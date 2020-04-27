1b3e3e9468fda2190d78be1d913274c9f335eb66
narendarreddypasham <narendarreddy0919@gmail.com>
2020-04-26 23:26:08 -0400

    M-1 commit

--- WcfService1/WcfService1/Service1.svc.cs
+++ WcfService1/WcfService1/Service1.svc.cs
@@ -16,7 +16,7 @@ namespace WcfService1
         {
             return string.Format("You entered: {0}", value);
         }
-
+        // this change from master
         public CompositeType GetDataUsingDataContract(CompositeType composite)
         {
             if (composite == null)
