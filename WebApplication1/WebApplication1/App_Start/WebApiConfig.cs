bf703f4f73cb7c9518995949490f6a3fc6e21f92
narendarreddypasham <narendarreddy0919@gmail.com>
2020-05-19 12:40:51 -0400

    commit

---
+++ WebApplication1/WebApplication1/App_Start/WebApiConfig.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web.Http;
+using Microsoft.Owin.Security.OAuth;
+using Newtonsoft.Json.Serialization;
+using System.Web.Http.OData.Builder;
+using WebApplication1.Models;
+using System.Web.Http.OData.Extensions;
+
+namespace WebApplication1
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+            // Configure Web API to use only bearer token authentication.
+
+            config.EnableCors();
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+            
+
+            ODataConventionModelBuilder builder = new ODataConventionModelBuilder();
+            builder.EntitySet<Employee>("Employees");
+            config.Routes.MapODataServiceRoute("odata", "odata", builder.GetEdmModel());
+            config.EnableQuerySupport(new QueryableAttribute());
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Controllers/EmployeesController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.ModelBinding;
+using System.Web.Http.OData;
+using System.Web.Http.OData.Routing;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    /*
+    The WebApiConfig class may require additional changes to add a route for this controller. Merge these statements into the Register method of the WebApiConfig class as applicable. Note that OData URLs are case sensitive.
+
+    using System.Web.Http.OData.Builder;
+    using System.Web.Http.OData.Extensions;
+    using WebApplication1.Models;
+    ODataConventionModelBuilder builder = new ODataConventionModelBuilder();
+    builder.EntitySet<Employee>("Employees");
+    config.Routes.MapODataServiceRoute("odata", "odata", builder.GetEdmModel());
+    */
+    public class EmployeesController : ODataController
+    {
+        private WebApicurdEntities db = new WebApicurdEntities();
+
+        // GET: odata/Employees
+        [EnableQuery]
+        public IQueryable<Employee> GetEmployees()
+        {
+            return db.Employees;
+        }
+
+        // GET: odata/Employees(5)
+        [EnableQuery]
+        public SingleResult<Employee> GetEmployee([FromODataUri] int key)
+        {
+            return SingleResult.Create(db.Employees.Where(employee => employee.EmployeeId == key));
+        }
+
+        // PUT: odata/Employees(5)
+        public IHttpActionResult Put([FromODataUri] int key, Delta<Employee> patch)
+        {
+            Validate(patch.GetEntity());
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            Employee employee = db.Employees.Find(key);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            patch.Put(employee);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EmployeeExists(key))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return Updated(employee);
+        }
+
+        // POST: odata/Employees
+        public IHttpActionResult Post(Employee employee)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Employees.Add(employee);
+            db.SaveChanges();
+
+            return Created(employee);
+        }
+
+        // PATCH: odata/Employees(5)
+        [AcceptVerbs("PATCH", "MERGE")]
+        public IHttpActionResult Patch([FromODataUri] int key, Delta<Employee> patch)
+        {
+            Validate(patch.GetEntity());
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            Employee employee = db.Employees.Find(key);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            patch.Patch(employee);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EmployeeExists(key))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return Updated(employee);
+        }
+
+        // DELETE: odata/Employees(5)
+        public IHttpActionResult Delete([FromODataUri] int key)
+        {
+            Employee employee = db.Employees.Find(key);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            db.Employees.Remove(employee);
+            db.SaveChanges();
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool EmployeeExists(int key)
+        {
+            return db.Employees.Count(e => e.EmployeeId == key) > 0;
+        }
+    }
+}
