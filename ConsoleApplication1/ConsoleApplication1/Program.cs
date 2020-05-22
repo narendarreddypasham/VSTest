5b3ee911879b0848f24eb8baf192dae3923a6f37
narendarreddypasham <narendarreddy0919@gmail.com>
2020-05-22 11:27:39 -0400

    test commit

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var todaytime = DateTime.Now;
+            var futuretime = todaytime.AddDays(47);
+            var testtime = todaytime.ToShortDateString();
+            Console.WriteLine(todaytime.AddDays(-14));
+            Console.WriteLine(futuretime.AddDays(-14));
+            Console.WriteLine(testtime);
+            Console.ReadLine();
+        }
+    }
+}
